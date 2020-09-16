aa0482a1d3d23974ebac269f40e8a11e1bda5a8d
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-16 19:45:05 +0800

    updated siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MDB Starter`,
+    description: `Starter bootstraped with MDBootstrap React or Material Bootstrap ReactJS.`,
+    author: `Fred Garingo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

49a719fb0660a58a1beb287c557b76fe9d191f44
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 15:00:03 +0800

    404 page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,29 @@
+import { Link } from "gatsby";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import React from "react"
 import Layout from "../components/layout"
+import missing from '../images/missing.png';
 
 const NotFoundPage = () => (
   <Layout title="Page Not Found">
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <MDBContainer className="py-5 my-5">
+      <MDBRow className="align-items-center">
+        <MDBCol md="6" lg="6">
+          <div className="content text-center">
+            <h1 className="h1-responsive">Ooooopsiee</h1>
+            <p className="lead">Page not found. Sorry for the inconvenience we weren't able to find what you were looking for.</p>
+            <Link to="/">
+              <MDBBtn color="dark" className="text-capitalize">
+                Go Home
+              </MDBBtn>
+            </Link>
+          </div>
+        </MDBCol>
+        <MDBCol md="6" lg="6">
+          <img src={missing} alt="missing vector" className="w-100"/>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   </Layout>
 )
 

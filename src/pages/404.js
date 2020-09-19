e638671a7cb6ec8599ad2603eece6bb734bc2b84
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 15:08:42 +0800

    updated for build

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,27 +4,29 @@ import React from "react"
 import Layout from "../components/layout"
 import missing from '../images/missing.png';
 
-const NotFoundPage = () => (
-  <Layout title="Page Not Found">
-    <MDBContainer className="py-5 my-5">
-      <MDBRow className="align-items-center">
-        <MDBCol md="6" lg="6">
-          <div className="content text-center">
-            <h1 className="h1-responsive">Ooooopsiee</h1>
-            <p className="lead">Page not found. Sorry for the inconvenience we weren't able to find what you were looking for.</p>
-            <Link to="/">
-              <MDBBtn color="dark" className="text-capitalize">
-                Go Home
-              </MDBBtn>
-            </Link>
-          </div>
-        </MDBCol>
-        <MDBCol md="6" lg="6">
-          <img src={missing} alt="missing vector" className="w-100"/>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  </Layout>
-)
+const MissingPage = () => {
+  return (
+    <Layout title="Page Not Found">
+      <MDBContainer className="py-5 my-5">
+        <MDBRow className="align-items-center">
+          <MDBCol md="6" lg="6">
+            <div className="content text-center">
+              <h1 className="h1-responsive">Ooooopsiee</h1>
+              <p className="lead">Page not found. Sorry for the inconvenience we weren't able to find what you were looking for.</p>
+              <Link to="/">
+                <MDBBtn color="dark" className="text-capitalize">
+                  Go Home
+                </MDBBtn>
+              </Link>
+            </div>
+          </MDBCol>
+          <MDBCol md="6" lg="6">
+            <img src={missing} alt="missing vector" className="w-100"/>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </Layout>
+  )
+}
 
-export default NotFoundPage
+export default MissingPage
